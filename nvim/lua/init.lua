808329c154675f9d592b93432a34bd1e8e41ec5e
Mike Ulicny <MichaelAUlicny@gmail.com>
2022-04-08 22:05:20 -0400

    fix lsp diagnostics issue

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -94,7 +94,7 @@ local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-    -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- mappings
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
