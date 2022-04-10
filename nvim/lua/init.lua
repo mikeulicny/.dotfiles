95cdf9c7d81203a670a1a3f45908f24bf68d9878
Mike Ulicny <MichaelAUlicny@gmail.com>
2022-04-10 17:10:28 -0400

    Remove use of <Enter> from selection for auto-complete in neovim. It is not required and annoying when trying to input a line break while autocomplete hover box exists

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -72,8 +72,8 @@ cmp.setup({
     },
     mapping = {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true })
+        ["<C-n>"] = cmp.mapping.select_next_item()
+        -- ["<Tab>"] = cmp.mapping.confirm({ select = true })
     },
     sources = {
         { name = 'nvim_lsp' },
