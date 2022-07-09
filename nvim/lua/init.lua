0490d4287ce60d10e4227158e23faaa69137e1be
Mike Ulicny <MichaelAUlicny@gmail.com>
2022-07-08 23:40:29 -0400

    remove un-neccessary nvim-tree options

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,38 +1,9 @@
 -- neovim tree
 require'nvim-tree'.setup {
-
     -- disables the netrw completely
     disable_netrw   = true,
-    -- hijack netrw window on startup
-    hijack_netrw = true,
-    -- open the tree when running this setup function
-    open_on_setup = false,
-    -- will not open on setup if the filetype is in this list
-    ignore_ft_on_setup = {},
-    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-    open_on_tab = false,
     --hijack the cursor in the tree to put it at the start of the filename
-    hijack_cursor = false,
-    -- updates the root directory of the tree on 'DirChanged' (when you run ':cd' usually)
-    update_cwd = false,
-    -- update the focused file on 'BufEnter', un-collapse the folders recursively until it finds the file
-    update_focused_file = {
-        -- enables the feature
-        enable = false,
-        -- update the root directory of the tree to the one of the folders containing the file if the file is not under the current root directory
-        -- only relevant when 'update_focused_file.enable' is true
-        update_cwd = false,
-        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-        -- only relevant when 'update_focused_file.update_cwd' is true and 'update_focused_file.enable' is true
-        ignore_list = {}
-    },
-    -- configuration options for the system open command ('s' in the tree by default)
-    system_open = {
-        -- the command to run this, leaving as nil should work in most cases
-        cmd = nil,
-        -- the command arguments as a list
-        args = {}
-    },
+    hijack_cursor = true,
     view = {
         -- width of the window, can be either a number (columns) or a string in '%', for left or right side placement
         width = 30,
@@ -41,13 +12,6 @@ require'nvim-tree'.setup {
         -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
         side = 'left',
         signcolumn = "yes",
-        mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
-            custom_only = false,
-            -- list of mappings to set on the tree manually
-            list = {}
-        }
     },
     renderer = {
         add_trailing = false,
